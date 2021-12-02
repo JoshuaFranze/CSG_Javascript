51a9e5f7a011c0a4bd6e99b710ad7a7a02e62ec5
JoshuaFranze <jfranze06@gmail.com>
2021-12-02 09:25:34 +0000

    .

--- JS/test.js
+++ JS/test.js
@@ -1,14 +1,36 @@
 function setup() {
   canvas = createCanvas(450,450);
-  background('silver');
+  background('lightblue');
   canvas.parent('processing');
   //noLoop();
 }
 
-function draw() {
+function kerk(kleur) {
   noStroke();
-  fill('steelblue');
-  ellipse(0,0,800);
-  fill('deepskyblue');
-  ellipse(450,450,400);
+  fill(kleur);
+  rect(0,0,100,200);
+  fill('maroon');
+  triangle(0,0,50,-100,100,0);
+  fill('yellow');
+  ellipse(50,25,40);
+  fill('gray');
+  rect(25,125,50,75);
+}
+
+function zon() {
+  noStroke();
+  fill('yellow');
+  ellipse(0,0,200);
+}
+
+
+function draw() {
+  translate(50,250);
+  kerk('saddlebrown');
+  translate(125,0);
+  kerk('sienna');
+  translate(125,-0);
+  kerk('chocolate');
+  translate(-275,-250);
+  zon();
 }
